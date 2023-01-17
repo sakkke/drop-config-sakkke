d934d50cd8c5ec6af3eb81c2750d46b21d227788
sakkke <w32w64@gmail.com>
2023-01-17 18:08:12 +0900

    feat: update $PATH

--- dotfiles/.config/fish/user.fish
+++ dotfiles/.config/fish/user.fish
@@ -1,4 +1,4 @@
 set -gx PATH $HOME/.local/bin:$PATH
+set -gx PATH (find ~/.cache/drop/apps -maxdepth 1 -mindepth 1 -exec printf {}/bin'\n' \; | paste -sd :):"$PATH"
 
 set -gx GOPATH $HOME/.cache/drop/apps/go
-set -gx PATH $GOPATH/bin:$PATH
