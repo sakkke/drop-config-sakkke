e53cf5127cf80404cdb15c3eb8b96b012af87bd9
sakkke <w32w64@gmail.com>
2023-01-17 18:13:13 +0900

    feat: add fcitx5 and mozc

--- dotfiles/.config/fish/user.fish
+++ dotfiles/.config/fish/user.fish
@@ -2,3 +2,7 @@ set -gx PATH $HOME/.local/bin:$PATH
 set -gx PATH (find ~/.cache/drop/apps -maxdepth 1 -mindepth 1 -exec printf {}/bin'\n' \; | paste -sd :):"$PATH"
 
 set -gx GOPATH $HOME/.cache/drop/apps/go
+
+set -gx GTK_IM_MODULE fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx XMODIFIERS @im=fcitx
