5594e81cc3f5c0ef19799a596746c1e06a864111
sakkke <w32w64@gmail.com>
2023-01-17 18:01:03 +0900

    feat: remove MOZ_ENABLE_WAYLAND

--- dotfiles/.config/fish/user.fish
+++ dotfiles/.config/fish/user.fish
@@ -2,7 +2,3 @@ set -gx PATH $HOME/.local/bin:$PATH
 
 set -gx GOPATH $HOME/.cache/drop/apps/go
 set -gx PATH $GOPATH/bin:$PATH
-
-if test $XDG_SESSION_TYPE = wayland
-  set -gx MOZ_ENABLE_WAYLAND 1
-end
