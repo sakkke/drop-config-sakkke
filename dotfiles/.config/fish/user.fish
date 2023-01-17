1d71c042f00b8ef550d33079a136abda948795ba
sakkke <w32w64@gmail.com>
2023-01-17 19:00:46 +0900

    feat: add ~/.cargo/bin to $PATH

--- dotfiles/.config/fish/user.fish
+++ dotfiles/.config/fish/user.fish
@@ -1,6 +1,8 @@
 set -gx PATH $HOME/.local/bin:$PATH
 set -gx PATH (find ~/.cache/drop/apps -maxdepth 1 -mindepth 1 -exec printf {}/bin'\n' \; | paste -sd :):"$PATH"
 
+set -gx PATH $HOME/.cargo/bin:$PATH
+
 set -gx GOPATH $HOME/.cache/drop/apps/go
 
 set -gx GTK_IM_MODULE fcitx
